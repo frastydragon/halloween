06286a358aa07bd233f4dd9301768e997696b759
Ben Thompson <ben.bhompson@gmail.com>
2024-10-08 22:38:24 -0400

    adds sound effects and images to the win screen and scare screen

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,10 @@ class SpiderScreen extends StatefulWidget {
 }
 
 class Spiders extends State<SpiderScreen> {
+  final AudioPlayer _audioPlayer1 = AudioPlayer();
+  final AudioPlayer _audioPlayer2 = AudioPlayer();
+  final AudioPlayer _audioPlayer3 = AudioPlayer();
+  final AudioPlayer _audioPlayer4 = AudioPlayer();
   double _xPosition = 50;
   double _yPosition = 100;
   final int objectCount = 5; //Number of spiders
@@ -36,6 +40,7 @@ class Spiders extends State<SpiderScreen> {
   
   void initState() {
     super.initState();
+    _loadSounds();
     // Start moving the object randomly every second
     Timer.periodic(const Duration(seconds: 1 ), (Timer timer) {
       setState(() { 
@@ -50,20 +55,64 @@ class Spiders extends State<SpiderScreen> {
     });
   }
 
-   void _onObjectTappedScare() {
+  Future<void> _loadSounds() async {
+    await Future.wait([
+      _audioPlayer1.setAsset('assets/squish.mp3'),
+      _audioPlayer2.setAsset('assets/scream.mp3'),
+      _audioPlayer3.setAsset('assets/Ghost House.mp3'),
+      _audioPlayer4.setAsset('assets/Spooky.mp3'),
+    ]);
+    _audioPlayer3.play();
+  }
+
+  void _playSound(int soundIndex) {
+    switch (soundIndex) {
+      case 1:
+        _audioPlayer1.seek(Duration.zero);
+        _audioPlayer1.play();
+        break;
+      case 2:
+        _audioPlayer2.seek(Duration.zero);
+        _audioPlayer2.play();
+        break;
+      case 3:
+        _audioPlayer3.seek(Duration.zero);
+        _audioPlayer3.play();
+        break;
+      case 4:
+        _audioPlayer4.seek(Duration.zero);
+        _audioPlayer4.play();
+        break;
+    }
+  }
+
+  void _onObjectTappedScare() {
     // Navigate to a different screen when the object is tapped
+    _playSound(2);
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => ScareScreen()),
     );
   }
 
-    void _onObjectTappedWin() {
+  void _onObjectTappedWin() {
     // Navigate to a different screen when the object is tapped
+    _playSound(1);
+    _audioPlayer3.stop();
+    _audioPlayer4.play();
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => WinScreen()),
     );
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer1.dispose();
+    _audioPlayer2.dispose();
+    _audioPlayer3.dispose();
+    _audioPlayer4.dispose();
+    super.dispose();
   }
 
   @override
@@ -111,14 +160,25 @@ class ScareScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Second Screen"),
+        title: const Text("BOO!!!"),
       ),
-      body: const Center(
-        child: Text("You clicked on the object!"),
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You clicked a Ghost!'),
+            Image.asset(
+              'assets/ghost.png',
+              height: 200,
+              width: 200,
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
 class WinScreen extends StatelessWidget {
   const WinScreen({super.key});
 
@@ -126,10 +186,20 @@ class WinScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Second Screen"),
+        title: const Text("NICE!!!"),
       ),
-      body: const Center(
-        child: Text("You clicked on the object!"),
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You squished the spider!'),
+            Image.asset(
+              'assets/skeletons.png',
+              height: 200,
+              width: 200,
+            )
+          ],
+        ),
       ),
     );
   }
